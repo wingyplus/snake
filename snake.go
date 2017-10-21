dec028c7b4744aa8d56f788ee6bfa4c2149534b5
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2017-10-21 12:10:30 +0700

    Growing snake tail when ate food

--- snake.go
+++ snake.go
@@ -17,6 +17,9 @@ type Snake struct {
 	scale float64
 	imd   *imdraw.IMDraw
 	win   *pixelgl.Window
+
+	total int
+	tail  []pixel.Vec
 }
 
 func (s *Snake) SetDirection(x, y float64) {
@@ -25,6 +28,11 @@ func (s *Snake) SetDirection(x, y float64) {
 }
 
 func (s *Snake) Update() {
+	for i := 0; i < s.total-1; i++ {
+		s.tail[i] = s.tail[i+1]
+	}
+	s.tail[s.total-1] = pixel.V(s.x, s.y)
+
 	s.x = s.x + s.xs*s.scale
 	s.y = s.y + s.ys*s.scale
 
@@ -36,9 +44,15 @@ func (s *Snake) Update() {
 
 func (s *Snake) Show(win pixel.Target) {
 	s.imd.Clear()
-	s.imd.SetMatrix(pixel.IM.Moved(pixel.V(s.x, s.y)))
-	s.imd.Push(pixel.V(0, 0), pixel.V(s.scale, s.scale))
-	s.imd.Rectangle(0)
+	for i := 0; i < s.total; i++ {
+		t := s.tail[i]
+		x, y := t.XY()
+
+		s.imd.SetMatrix(pixel.IM.Moved(pixel.V(x, y)))
+		s.imd.Push(pixel.V(0, 0), pixel.V(s.scale, s.scale))
+		s.imd.Rectangle(0)
+	}
+
 	s.imd.Draw(win)
 }
 
@@ -50,7 +64,17 @@ func (s *Snake) Eat(food *Food) bool {
 	fx, fy := food.Pos()
 	sx, sy := s.Pos()
 	d := mathutil.Dist(sx, sy, fx, fy)
-	return d < 1
+
+	// when snake ate food, create tail by create new slice and copy from old slice to new slice
+	if d < 1 {
+		s.total++
+		old := s.tail
+		new := make([]pixel.Vec, s.total, s.total+1)
+		copy(new, old)
+		s.tail = new
+		return true
+	}
+	return false
 }
 
 func NewSnake(scale float64, win *pixelgl.Window) *Snake {
@@ -59,5 +83,7 @@ func NewSnake(scale float64, win *pixelgl.Window) *Snake {
 		scale: scale,
 		imd:   imdraw.New(nil),
 		win:   win,
+		total: 1,
+		tail:  make([]pixel.Vec, 1),
 	}
 }
